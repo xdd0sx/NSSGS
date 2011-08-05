ebd8a76e767cf0314b0755035b8a7a81263e94a0
xdd0sx <key@mul.tiver.se>
2011-08-05 18:52:18 +0800

    x

---
+++ compass/config.rb
@@ -0,0 +1,8 @@
+http_path    = "/"
+project_path = "."
+css_dir      = "output/stylesheet"
+sass_dir     = "content/stylesheet"
+images_dir   = "output/images"
+javascripts_dir = "output/js"
+
+preferred_syntax = :sass
